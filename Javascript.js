8d8fa7c3b0abb8da86e8fcb764c5eab8d5593258
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-06 00:24:45 +0800

    wew

--- Javascript.js
+++ Javascript.js
@@ -3,6 +3,12 @@ var http = require('http');
 var hostname = '192.168.254.157';
 var port = 8000;
 
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
