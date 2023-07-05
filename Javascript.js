c282b4fbcb726870f1048c9f61c04d6a48b43b71
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-06 00:34:31 +0800

    added the html file

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '192.168.254.157';
 var port = 8000;
 
 var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  fs.readFile('/opt/Javascript/JavaScript-Application/index.html', (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Internal Server Error');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
