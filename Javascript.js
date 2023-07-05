3bab0c2f0d84c4f5d95f1e5552b38624c59a0e17
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-05 19:41:35 +0800

    u

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,14 @@
 var http = require('http');
-var hostname = '192.168.254.157';
+
+var hostname = 'localhost';
 var port = 8080;
+
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
