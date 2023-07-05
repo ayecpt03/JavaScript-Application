50d4f1bde19a3b51cd3bd8368970c508a8a729aa
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-05 19:03:04 +0800

    Updated the .js file

--- Javascript.js
+++ Javascript.js
@@ -1,15 +1,5 @@
 const http = require('http');
-const port = 3000; // Port number
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  // Request handling logic
-});
+http.listen(port, () => console.log(`Server running on port ${port}`));
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-function displayMessage() {
-    var messageElement = document.getElementById("message");
-    messageElement.textContent = "Hello, World!";
-}
