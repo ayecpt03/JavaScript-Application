d30830b55a0ecc856b5086917c08b028c336e027
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-05 19:18:43 +0800

    u

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,11 @@
-const app = require('../JavaScript-Application');
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`)); 
+var http = require('http');
+var hostname = 'localhost';
+var port = 8000;
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
