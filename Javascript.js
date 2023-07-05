d3243fb643dbfb1143f873234e6d724af3c68221
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-05 17:57:07 +0800

    Added port

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,14 @@
+const http = require('http');
+const port = 3000; // Port number
+
+const server = http.createServer((req, res) => {
+  // Request handling logic
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 function displayMessage() {
     var messageElement = document.getElementById("message");
     messageElement.textContent = "Hello, World!";
