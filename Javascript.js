d2671959864f1d3942a48babc8268dd05cc8adf4
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-07-11 00:00:45 +0800

    Update the file again

--- Javascript.js
+++ Javascript.js
@@ -22,13 +22,6 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// Get the button element by its id
-var button = document.getElementById('myButton');
-
-// Function to handle button click event
-function handleClick() {
-  alert('Button Clicked!');
-}
-
-// Add a click event listener to the button
-button.addEventListener('click', handleClick);
+document.getElementById('myButton').addEventListener('click', function() {
+  alert('Button clicked!');
+});
