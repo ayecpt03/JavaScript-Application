0abaae2a380263bda87de26d30de3d2501c6be49
Adrian Mendoza <ayecruz.mendoza@gmail.com>
2023-09-01 02:38:52 +0800

    Push

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const hostname = '192.168.254.157';
+const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
